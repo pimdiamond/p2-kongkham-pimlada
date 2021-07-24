04500d722ac40cd4550243857538d31ac23fe393
Pimlada <pimdiamond@gmail.com>
2021-07-24 18:52:26 -0400

    added Animated Collapsible (Slide Down) in js

--- js/main.js
+++ js/main.js
@@ -87,3 +87,21 @@ $('.slider').each(function() {
 
   advance();
 });
+
+
+// Added Animated Collapsible (Slide Down)
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
