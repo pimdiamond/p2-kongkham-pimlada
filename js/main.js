77af8157c09e6e24f1079348918664c08646c7c7
Pimlada <pimdiamond@gmail.com>
2021-07-24 20:06:10 -0400

    Added hamburger menu in js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 
+// Added hamburger menu
+$( document ).ready(function() {
+
+  $( ".cross" ).hide();
+  $( ".menu" ).hide();
+  $( ".hamburger" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+  $( ".hamburger" ).hide();
+  $( ".cross" ).show();
+  });
+  });
+  
+  $( ".cross" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+  $( ".cross" ).hide();
+  $( ".hamburger" ).show();
+  });
+  });
+  
+  });
 
 // Added Animated Collapsible (Slide Down)
 
